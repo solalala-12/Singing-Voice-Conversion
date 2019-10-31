04e312ac495fbef24bf64ebcef7b1e20899e312b
dizwe <dizwe2716@gmail.com>
2019-10-31 15:08:32 +0900

    ADD crawling youtube

---
+++ crawl_youtube/get_youtube.py
@@ -0,0 +1,41 @@
+#%% 
+import pandas as pd
+current_dir = 'sound/Tobigs_music_project/practice'
+class_df = pd.read_csv(f'{current_dir}/class_labels_indices.csv')
+# 데이터에 ,로 구분되는게 있어서 ", "로 구분되기 해야한다.
+balanced_df = pd.read_csv(f'{current_dir}/balanced_train_segments.csv',sep=", ")
+
+#%%
+baby_mid = class_df[class_df['display_name']=='Baby cry, infant cry']['mid']
+snore_mid = class_df[class_df['display_name']=='Snoring']['mid']
+# to_string 하면 index까지 같이 나옴
+# balanced_df[balanced_df["positive_labels"].str.find(baby_mid.to_string())!=-1]
+baby_df = balanced_df[balanced_df["positive_labels"].str.find(baby_mid.values[0])!=-1]
+snore_df = balanced_df[balanced_df["positive_labels"].str.find(snore_mid.values[0])!=-1]
+
+#%%
+from pytube import YouTube
+import subprocess
+import os
+## iter_rows 해줘야 함
+for index, sr in snore_df.iterrows():
+    print(sr)
+    print(f'https://www.youtube.com/watch?v={sr["YTID"]}')
+    yt = YouTube(f'https://www.youtube.com/watch?v={sr["YTID"]}')
+    vids = yt.streams.filter(file_extension='mp4').all() 
+    # print(vids)
+    # [<Stream: itag="140" mime_type="audio/mp4" abr="128kbps" acodec="mp4a.40.2">, <Stream: itag="249" mime_type="audio/webm" abr="50kbps" acodec="opus">, <Stream: itag="250" mime_type="audio/webm" abr="70kbps" acodec="opus">, <Stream: itag="251" mime_type="audio/webm" abr="160kbps" acodec="opus">]
+    vids[0].download(f'{current_dir}/data/snore/')
+    print(vids[0].default_filename)
+    # ffmpeg -ss (start time) -i (direct video link) -t (duration needed) -c:v copy -c:a copy (destination file)
+
+    ### Overwrite 안됨.
+    subprocess.call(['ffmpeg','-ss',sr["start_seconds"],'-i',
+        os.path.join(f'{current_dir}/data/snore/' ,vids[0].default_filename),
+        '-t',sr["start_second"]-sr["end_second"],
+        os.path.join(f'{current_dir}/data/snore/',"a.mp3")
+    ])
+    print(1111)
+
+    # ffmpeg -ss 10 -i kk.mp4 -t 30 a.mp3
+#%%
